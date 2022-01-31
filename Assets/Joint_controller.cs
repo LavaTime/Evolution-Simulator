ef5d43bf5a51e5072ceffc57b9381060e068f6ff
Amit Keren <amitkeren9@gmail.com>
2022-01-31 22:02:54 +0200

    Ground Tilemap and Joint weight color
    Added some sprites and sorted the tile palette.
    Started creating the Joint class. Made a weight slider to change the color according to the pattern: White => Red => Black

---
+++ Assets/Joint_controller.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joint_controller : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float weight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //        --------------------------------------- MOVE TO Start() -----------------------------------------
+        int colorInt = (int) ((1 - this.weight) * 765);
+        // Change into White => Red => Black colors
+        float R_value = ((float) colorInt) / 255f;
+        float GB_value  = 0f;
+        if (colorInt > 255)
+        {
+            GB_value = ((float) ((colorInt - 255) / 2))/255f;
+            R_value = 1f;
+        }
+        Color newColor = new Color(R_value, GB_value, GB_value);
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = newColor;
+    }
+}
